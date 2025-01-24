32545665f729e8072529187fb55f0f5143b67da9
harikrishnan-git <haribkrishh@gmail.com>
2025-01-25 04:30:58 +0530

    added tailwindcss and created login page

--- my-react-app/src/Components/LoginBg.jsx
+++ my-react-app/src/Components/LoginBg.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function LoginBg() {
   return (
-    <div className="bg-[url(login-bg.png)] rounded-r-lg loginbg bg-cover w-auto h-auto bg-no-repeat"></div>
+    <div className="bg-[url(/images/login-bg.png)] rounded-r-lg loginbg bg-cover w-auto h-auto bg-no-repeat"></div>
   );
 }
